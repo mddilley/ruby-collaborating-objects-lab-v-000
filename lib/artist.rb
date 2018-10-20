48f6c4e59c3d0906a5baabd5fb1181fa19ac8d14
Mike Dilley <mike.dilley@gmail.com>
2018-10-20 16:03:08 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
 
   attr_accessor :name
-  
+
   def initialize(name)
     @name = name
     @songs = []
@@ -10,6 +10,7 @@ class Artist
   def add_song(song)
     # This method will:
     # 1. Store the Song object passed in to the method in @songs array
+    @songs << song
   end
 
 end
