20863c8b5460d7cf44c49c1c4c76263017c29f3f
Mike Dilley <mike.dilley@gmail.com>
2018-10-20 15:55:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,18 @@ class Song
   end
 
   def artist=(name)
+    # This method will:
+    # 1. Turn artist string passed in this method into an Artist object
+    # 2. New Artist object will be assigned to @artist
     @artist = Artist.find_or_create_by_name(name)
-    
+    # 3. Assign this song instance to the songs in the artist object
+        
+  end
+
+  def self.new_by_filename(file_name)
+    # This method will:
+    # 1. Parse the file name
+    parsed_filename = file_name.split(" - ")
   end
 
 end
